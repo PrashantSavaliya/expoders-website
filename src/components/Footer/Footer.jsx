f70f474d6f46e47a2e2e1e8c23c3b4a5ae05478a
PrashantSavaliya <125353350+PrashantSavaliya@users.noreply.github.com>
2024-06-10 12:48:37 +0530

    ji

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
         return location.pathname === path || (path !== '/' && location.pathname.startsWith(path)) ? 'active' : '';
       };
     return (
-        <>
+        <div className='footer-top'>
             <div className='footer container'>
                 <div className="footer-about-us">
                     <h1>EXPODERS SOLUTIONS</h1>
@@ -67,8 +67,8 @@ const Footer = () => {
                         <li><FaSkype className='contact-icons' /> dashu6101</li>
                         <li><a href="mailto:info@expoders.com"><IoMailOutline className='contact-icons' /> info@expoders.com</a></li>
                         <li><a href="tel:+918460311248"><FaPhone className='contact-icons' /> (+91) 8460 311248</a></li>
-                        <li><FaLocationDot className='contact-icons' /> 503, Horizon Trade Center, Vaishnovdevi circle Besides KD hospital, Sarkhej - Gandhinagar Highway, Ahmedabad, Gujarat 382481</li>
-                        <li><FaLocationDot className='contact-icons' /> F/25, Ashapura complex, Detroj Road, Kadi 382715</li>
+                        <li><a href="https://maps.app.goo.gl/rqyeCDuLJsvG3FZX9" target='_blank'><FaLocationDot className='contact-icons' /> 503, Horizon Trade Center, Vaishnovdevi circle Besides KD hospital, Sarkhej - Gandhinagar Highway, Ahmedabad, Gujarat 382481</a></li>
+                        <li><a href="https://maps.app.goo.gl/W5Waj47T3pYuQiMw6" target='_blacnk'><FaLocationDot className='contact-icons' /> F/25, Ashapura complex, Detroj Road, Kadi 382715</a></li>
                     </ul>
                 </div>
             </div>
@@ -76,7 +76,7 @@ const Footer = () => {
                 <img src={expoders_logo} alt="" />
                 <p>© 2024 <span>Consen</span> all rights reserved. Design By Prashant Savavliya.</p>
             </div>
-        </>
+        </div>
     )
 }
 
