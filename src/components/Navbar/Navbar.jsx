7a32416039db0c020a1e6e4e8f53793e53ccb06a
PrashantSavaliya <125353350+PrashantSavaliya@users.noreply.github.com>
2024-06-12 15:34:18 +0530

    dsf

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,70 @@
+// import React, { useEffect, useState } from 'react'
+// import './Navbar.css';
+// import expoders_img from '../../assets/Expoders_logo2.png';
+// import { MdMenu } from "react-icons/md";
+// import { FaCaretDown } from "react-icons/fa";
+// import { Link, useLocation } from "react-router-dom";
+// 
+// const Navbar = ({ dark_nav }) => {
+//   const [sticky, setSticky] = useState(false)
+//   const [MobileMenu, setMobileMenu] = useState(false)
+//   const location = useLocation();
+// 
+//   useEffect(() => {
+//     window.addEventListener('scroll', () => {
+//       window.scrollY > 50 ? setSticky(true) : setSticky(false)
+//     })
+//   }, [])
+// 
+//   const toggleMenu = () => {
+//     setMobileMenu(!MobileMenu);
+//   }
+// 
+//   const closeMenu = () => {
+//     setMobileMenu(false);
+//   }
+// 
+//   const isActive = (path) => {
+//     return location.pathname === path || (path !== '/' && location.pathname.startsWith(path)) ? 'active' : '';
+//   };
+// 
+//   return (
+//     <nav className='container dark-nav'>
+//       <Link to='/'><img src={expoders_img} alt="" className='logo' /></Link>
+//       <ul className={MobileMenu ? '' : 'hide-mobile-icon'}>
+//         <li className={isActive('/')} onClick={closeMenu}>
+//           <Link to='/'>Home</Link>
+//         </li>
+//         <li className={isActive('/company')}>
+//           <Link to='/company/about-expoders'>Company <FaCaretDown /></Link>
+//           <ul>
+//             <li className={isActive('/company/about-expoders')} onClick={closeMenu}><Link to='/company/about-expoders'>About Expoders</Link></li>
+//             <li className={isActive('/company/about-team')} onClick={closeMenu}><Link to='/company/about-team'>About Team</Link></li>
+//             <li className={isActive('/company/about-methodology')} onClick={closeMenu}><Link to='/company/about-methodology'>Methodology</Link></li>
+//           </ul>
+//         </li>
+//         <li className={isActive('/services')}>
+//           <Link to='/services'>Service <FaCaretDown /></Link>
+//           <ul>
+//             <li onClick={closeMenu}><Link to='/services'>WEB DEVELOPMENT</Link></li>
+//             <li onClick={closeMenu}><Link to='/services'>MOBILE APP DEVELOPMENT</Link></li>
+//             <li onClick={closeMenu}><Link to='/services'>DESIGN AND CNS</Link></li>
+//             <li onClick={closeMenu}><Link to='/services'>OTHER IT SERVICES</Link></li>
+//           </ul>
+//         </li>
+//         <li className={isActive('/portfolio')} onClick={closeMenu}><Link to='/portfolio'>Portfolio</Link></li>
+//         <li className={isActive('/hire-developer')} onClick={closeMenu}><Link to='/hire-developer'>Hire Developers</Link></li>
+//         <li className={isActive('/career')} onClick={closeMenu}><Link to='/career'>Career</Link></li>
+//         <li className={isActive('/blog')} onClick={closeMenu}><Link to='/blog'>Blog</Link></li>
+//         <li onClick={closeMenu}><button className='btn'><Link to='/contact-us'>Contact Us</Link></button></li>
+//       </ul>
+//       <MdMenu className='menu-icon' onClick={toggleMenu} />
+//     </nav>
+//   )
+// }
+// 
+// export default Navbar
+
 import React, { useEffect, useState } from 'react'
 import './Navbar.css';
 import expoders_img from '../../assets/Expoders_logo2.png';
@@ -8,6 +75,8 @@ import { Link, useLocation } from "react-router-dom";
 const Navbar = ({ dark_nav }) => {
   const [sticky, setSticky] = useState(false)
   const [MobileMenu, setMobileMenu] = useState(false)
+  const [companySubmenuOpen, setCompanySubmenuOpen] = useState(true); 
+  const [serviceSubmenuOpen, setserviceSubmenuOpen] = useState(true); 
   const location = useLocation();
 
   useEffect(() => {
@@ -22,6 +91,16 @@ const Navbar = ({ dark_nav }) => {
 
   const closeMenu = () => {
     setMobileMenu(false);
+    setCompanySubmenuOpen(false); 
+    setserviceSubmenuOpen(false); 
+  }
+
+  const toggleCompanySubmenu = () => {
+    setCompanySubmenuOpen(!companySubmenuOpen); 
+  }
+
+  const toggleServiceSubmenu = () => {
+    setserviceSubmenuOpen(!serviceSubmenuOpen); 
   }
 
   const isActive = (path) => {
@@ -36,16 +115,16 @@ const Navbar = ({ dark_nav }) => {
           <Link to='/'>Home</Link>
         </li>
         <li className={isActive('/company')}>
-          <Link to='/company/about-expoders'>Company <FaCaretDown /></Link>
-          <ul>
+          <span onClick={toggleCompanySubmenu}>Company <FaCaretDown /></span>
+          <ul className={companySubmenuOpen ? 'show' : ''}>
             <li className={isActive('/company/about-expoders')} onClick={closeMenu}><Link to='/company/about-expoders'>About Expoders</Link></li>
             <li className={isActive('/company/about-team')} onClick={closeMenu}><Link to='/company/about-team'>About Team</Link></li>
             <li className={isActive('/company/about-methodology')} onClick={closeMenu}><Link to='/company/about-methodology'>Methodology</Link></li>
           </ul>
         </li>
         <li className={isActive('/services')}>
-          <Link to='/services'>Service <FaCaretDown /></Link>
-          <ul>
+        <span onClick={toggleServiceSubmenu}>Services <FaCaretDown /></span>
+        <ul className={serviceSubmenuOpen ? 'show' : ''}>
             <li onClick={closeMenu}><Link to='/services'>WEB DEVELOPMENT</Link></li>
             <li onClick={closeMenu}><Link to='/services'>MOBILE APP DEVELOPMENT</Link></li>
             <li onClick={closeMenu}><Link to='/services'>DESIGN AND CNS</Link></li>
@@ -63,4 +142,4 @@ const Navbar = ({ dark_nav }) => {
   )
 }
 
-export default Navbar
+export default Navbar;
