88e478a110fa5b79e1f0320d1e6b4e92ddb59abb
PrashantSavaliya <125353350+PrashantSavaliya@users.noreply.github.com>
2024-06-12 12:14:57 +0530

    dsf

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,45 +16,46 @@ const Navbar = ({ dark_nav }) => {
   }, [])
 
   const toggleMenu = () => {
-    MobileMenu ? setMobileMenu(false) : setMobileMenu(true)
+    setMobileMenu(!MobileMenu);
+  }
+
+  const closeMenu = () => {
+    setMobileMenu(false);
   }
 
   const isActive = (path) => {
-    // return location.pathname === path ? 'active' : '';
-    // return location.pathname.startsWith(path) ? 'active' : '';
     return location.pathname === path || (path !== '/' && location.pathname.startsWith(path)) ? 'active' : '';
   };
 
   return (
-    // <nav className={`container ${sticky ? dark_nav : 'dark-nav'}`}>
     <nav className='container dark-nav'>
-      <Link to = '/'><img src={expoders_img} alt="" className='logo' /></Link>
+      <Link to='/'><img src={expoders_img} alt="" className='logo' /></Link>
       <ul className={MobileMenu ? '' : 'hide-mobile-icon'}>
-        <li className={isActive('/')}>
+        <li className={isActive('/')} onClick={closeMenu}>
           <Link to='/'>Home</Link>
         </li>
         <li className={isActive('/company')}>
-          <Link to='/company/about-expoders'>Company</Link>
+          <Link to='/company/about-expoders' onClick={closeMenu}>Company</Link>
           <ul>
-            <li className={isActive('/company/about-expoders')}><Link to='/company/about-expoders'>About Expoders</Link></li>
-            <li className={isActive('/company/about-team')}><Link to='/company/about-team'>About Team</Link></li>
-            <li className={isActive('/company/about-methodology')}><Link to='/company/about-methodology'>Methodology</Link></li>
+            <li className={isActive('/company/about-expoders')} onClick={closeMenu}><Link to='/company/about-expoders'>About Expoders</Link></li>
+            <li className={isActive('/company/about-team')} onClick={closeMenu}><Link to='/company/about-team'>About Team</Link></li>
+            <li className={isActive('/company/about-methodology')} onClick={closeMenu}><Link to='/company/about-methodology'>Methodology</Link></li>
           </ul>
         </li>
         <li className={isActive('/services')}>
-          <Link to='/services'>Service</Link>
+          <Link to='/services' onClick={closeMenu}>Service</Link>
           <ul>
-            <li><Link to='/services'>WEB DEVELOPMENT</Link></li>
-            <li><Link to='/services'>MOBILE APP DEVELOPMENT</Link></li>
-            <li><Link to='/services'>DESIGN AND CNS</Link></li>
-            <li><Link to='/services'>OTHER IT SERVICES</Link></li>
+            <li onClick={closeMenu}><Link to='/services'>WEB DEVELOPMENT</Link></li>
+            <li onClick={closeMenu}><Link to='/services'>MOBILE APP DEVELOPMENT</Link></li>
+            <li onClick={closeMenu}><Link to='/services'>DESIGN AND CNS</Link></li>
+            <li onClick={closeMenu}><Link to='/services'>OTHER IT SERVICES</Link></li>
           </ul>
         </li>
-        <li className={isActive('/portfolio')}><Link to='/portfolio'>Portfolio</Link></li>
-        <li className={isActive('/hire-developer')}><Link to='/hire-developer'>Hire Developers</Link></li>
-        <li className={isActive('/career')}><Link to='/career'>Career</Link></li>
-        <li className={isActive('/blog')}><Link to='/blog'>Blog</Link></li>
-        <li><button className='btn'><Link to='/contact-us'>Contact Us</Link></button></li>
+        <li className={isActive('/portfolio')} onClick={closeMenu}><Link to='/portfolio'>Portfolio</Link></li>
+        <li className={isActive('/hire-developer')} onClick={closeMenu}><Link to='/hire-developer'>Hire Developers</Link></li>
+        <li className={isActive('/career')} onClick={closeMenu}><Link to='/career'>Career</Link></li>
+        <li className={isActive('/blog')} onClick={closeMenu}><Link to='/blog'>Blog</Link></li>
+        <li onClick={closeMenu}><button className='btn'><Link to='/contact-us'>Contact Us</Link></button></li>
       </ul>
       <MdMenu className='menu-icon' onClick={toggleMenu} />
     </nav>
